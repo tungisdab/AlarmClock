f9f55b4b0ad0ac5015daddfc71aa848c0dcad4f0
tungisdab <khanhdzno1st01061102@gmail.com>
2023-09-21 07:45:52 +0700

    update

--- lib/page/lovedays.dart
+++ lib/page/lovedays.dart
@@ -11,6 +11,7 @@ class _LovedaysState extends State<Lovedays> with AutomaticKeepAliveClientMixin{
   @override
   bool get wantKeepAlive => true;
 
+  int daylove = 1000;
   int currentPageIndex = 0;
 
   @override
@@ -47,57 +48,111 @@ class _LovedaysState extends State<Lovedays> with AutomaticKeepAliveClientMixin{
 
   Widget loveDay(){
     return SafeArea(
-      child: Column(
-        children: [
-          Padding(
-            
-            padding: const EdgeInsets.only(
-              top: 20,
-              bottom: 20,
-            ),
-            child: Text(
-              'Celebrate love on a day of your choice',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              
+              padding: const EdgeInsets.only(
+                top: 20,
+                bottom: 20,
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 20,
-              bottom: 20,
-              left: 40,
-              right: 40,
-            ),
-            child: TextField(
-              readOnly: true,
-              decoration: InputDecoration(
-                hintText: 'Celebrate love',
-                fillColor: Colors.red,
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime(2150),
-                    );
-                  },
-                  icon: const Icon(Icons.calendar_today),
+              child: Text(
+                'Celebrate love on a day of your choice',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(35),
-            child: Image.asset(
-              'assets/icon/icon.png',
-              fit: BoxFit.cover,
-            )
-          ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+                left: 40,
+                right: 40,
+              ),
+              child: TextField(
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Celebrate love',
+                  fillColor: Colors.red,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime(2150),
+                      );
+                    },
+                    icon: const Icon(Icons.calendar_today),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [       
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/2,
+                    height: MediaQuery.of(context).size.width/2,
+                    child: Image.asset('assets/icon/icon.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '$daylove',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'days of love',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.all(30),
+              child: Text(
+                'My love for you is greater than $daylove days of love. I love you more than anything in the world.',
+                overflow: TextOverflow.visible,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ),
+            Container(
+              margin: EdgeInsets.all(35),
+              child: Image.asset(
+                'assets/icon/icon.png',
+                fit: BoxFit.cover,
+              )
+            ),
+            Container(
+              margin: EdgeInsets.all(35),
+              child: Image.asset(
+                'assets/icon/icon.png',
+                fit: BoxFit.cover,
+              )
+            ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
