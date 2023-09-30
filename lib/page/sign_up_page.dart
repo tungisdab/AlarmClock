2cb3d4de7b78a315cb159d86f86a6a6c75ada276
tungisdab <khanhdzno1st01061102@gmail.com>
2023-09-30 23:06:34 +0700

    update sign up

--- lib/page/sign_up_page.dart
+++ lib/page/sign_up_page.dart
@@ -14,19 +14,184 @@ class _SignUpPageState extends State<SignUpPage> {
     double heightScreen = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            width: widthScreen, 
-            height: heightScreen * 0.3,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/icon/icon.png'),
-                fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: widthScreen, 
+              height: heightScreen * 0.3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/a8.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          )
-        ],
+            Container(
+              width: widthScreen,
+              margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hello', 
+                    style: TextStyle(
+                      fontSize: 60, 
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  Text(
+                    'Sign into your account',  
+                    style: TextStyle(
+                      fontSize: 20, 
+                      color: Colors.grey
+                    )
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          offset: Offset(1, 1),
+                          spreadRadius: 7,
+                          color: Colors.grey.withOpacity(0.2)
+                        )
+                      ]
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          offset: Offset(1, 1),
+                          spreadRadius: 7,
+                          color: Colors.grey.withOpacity(0.2)
+                        )
+                      ]
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                            width: 1,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        'Forgot your password',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),  
+            Container(
+              width: widthScreen * 0.5,
+              height: heightScreen * 0.08,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/a8.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: heightScreen * 0.08,
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'Don\'t have an account? ',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Sign up',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
